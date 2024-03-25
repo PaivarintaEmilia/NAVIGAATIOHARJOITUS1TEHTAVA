aea67761261ecb4fda444019a90d4fdf4c047d09
Maria Emilia Päivärinta <eemilia.paivarinta@gmail.com>
2024-03-25 18:28:11 +0100

    Siistittiin turhat tiedostot pois.

--- app/src/main/java/com/example/navigaatioharjoitus1tehtava/viewModels/ScreenBViewModel.kt
+++
@@ -1,4 +0,0 @@
-package com.example.navigaatioharjoitus1tehtava.viewModels
-
-class ScreenBViewModel {
-}
--- app/src/main/java/com/example/navigaatioharjoitus1tehtava/viewModels/ScreenCViewModel.kt
+++
@@ -1,9 +0,0 @@
-package com.example.navigaatioharjoitus1tehtava.viewModels
-
-import androidx.compose.runtime.mutableStateOf
-import androidx.lifecycle.ViewModel
-
-class ScreenCViewModel : ViewModel() {
-
-
-}
